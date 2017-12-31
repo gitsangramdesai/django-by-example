aed5bcf86b1921db4b252591009b0561fd35f18f
sangram <mail.sangram.desai@gmail.com>
2017-12-31 18:44:00 +0530

    second commit

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+            self).get_queryset()\
+            .filter(status='published') 
 
 class Post(models.Model):
         STATUS_CHOICES = (
@@ -20,10 +25,22 @@ class Post(models.Model):
         status = models.CharField(max_length=10, 
                                 choices=STATUS_CHOICES, 
                                 default='draft')
+        
+        objects = models.Manager() # The default manager.
+        published = PublishedManager() # Our custom manager.
+
+        def get_absolute_url(self):
+            return reverse('blog:post_detail',
+                args=[self.publish.year,
+                self.publish.strftime('%m'),
+                self.publish.strftime('%d'),
+                self.slug])
 
 
-        class Meta:
-            ordering = ('-publish',)
+class Meta:
+    ordering = ('-publish',)
 
-        def __str__(self):
-            return self.title
+def __str__(self):
+    return self.title
+
+       
